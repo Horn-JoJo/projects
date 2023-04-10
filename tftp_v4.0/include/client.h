56c2b7f4773a122c74cad9bf3bd03e58025bc794
“Horn-JoJo” <1494484196@qq.com>
2023-04-10 20:12:38 +0800

    初步完结优化ls并添加到tftp服务器中，下一步：实现登录以及注册的功能

---
+++ tftp_v4.0/include/client.h
@@ -0,0 +1,19 @@
+#ifndef _CLIENT_H
+#define _CLIENT_H
+#include "net.h"
+
+#define SIZE 128
+void do_get(char []);//从服务器端下载文件
+void do_put(char []);//上传文件到服务器端
+void do_list(char []);//显示服务器给定客户端的目录内容
+void do_help();//离线帮助手册
+void do_log_in();//实现登录操作
+void do_sign_up();//实现注册操作
+
+#define err_log(log)\
+	do{\
+		perror(log);\
+		return;\
+	}while(0);
+#endif
+
